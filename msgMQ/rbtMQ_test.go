44b44fad33558e76d6ba09e3b68887024f667ee1
wuxun <84924650@qq.com>
2020-05-22 21:07:12 +0800

    rabbitmq增加proto数据测试

--- msgMQ/rbtMQ_test.go
+++ msgMQ/rbtMQ_test.go
@@ -8,6 +8,8 @@ package msgMQ_test
 
 import (
 	"fmt"
+	"github.com/golang/protobuf/proto"
+	"tcpFrame/datas/proto"
 	"tcpFrame/msgMQ"
 	"tcpFrame/util"
 	"testing"
@@ -16,7 +18,13 @@ import (
 func Test_rabbitMq(t *testing.T) {
 	err := msgMQ.BindServiceQueue("server1", "exchangeName1", "hello", "rbt.key1")
 	fmt.Println(util.RunFuncName(), err)
-	err = msgMQ.Publish2Service("server1", "exchangeName1", "rbt.key1", []byte("hello world!"))
+	dp := &heartbeat.LoginRequest{
+		UserName:"wuxun",
+		Password:"123456",
+		LoginType:1,
+	}
+	db, _ := proto.Marshal(dp)
+	err = msgMQ.Publish2Service("server1", "exchangeName1", "rbt.key1", db)
 	fmt.Println(util.RunFuncName(), err)
 	err = msgMQ.AddConsumeMsg("server1", "hello", "consumer1")
 	fmt.Println(util.RunFuncName(), err)
@@ -25,6 +33,9 @@ func Test_rabbitMq(t *testing.T) {
 		fmt.Println(util.RunFuncName(), err, "没有数据或连接!")
 	}else{
 		message := <- rbtMsg
-		fmt.Println("get message : ", string(message.Body))
+		fmt.Println("get message : ", message.Body)
+		dp2 := &heartbeat.LoginRequest{}
+		err = proto.Unmarshal(message.Body, dp2)
+		fmt.Println("dp2: ", dp2, " err: ", err)
 	}
 }
